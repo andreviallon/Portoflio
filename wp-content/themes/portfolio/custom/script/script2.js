9ee30f03f33d84c7f55130db09757c5635a1eba3
andreviallon <viallon.andre@gmail.com>
2018-01-13 14:23:17 +0100

    mobile menu almost done

--- wp-content/themes/portfolio/custom/script/script2.js
+++ wp-content/themes/portfolio/custom/script/script2.js
@@ -1,6 +1,27 @@
 
 $(document).ready(function () {
 
+
+    //Menu 
+    //when menu is closed on click open it
+    $('.menu-mobile').click(function () {
+        $('.mobile-nav-toggle-cross').css({ 'display': 'block' });
+        $('.menu-mobile').css({ 'display': 'none' });
+        $('.menu-mobile-content-container').css({ 'display': 'flex' });
+    });
+    //when menu is open on click close it
+    $('.mobile-menu-nav .closing-cross-container').click(function () {
+        $('.mobile-nav-toggle-cross').css({ 'display': 'none' });
+        $('.menu-mobile').css({ 'display': 'block' });
+        $('.menu-mobile-content-container').css({ 'display': 'none' });
+    });
+    //when menu is open adn click on menu item close menu
+    $('.mobile-nav-li a').click(function () {
+        $('.mobile-nav-toggle-cross').css({ 'display': 'none' });
+        $('.menu-mobile').css({ 'display': 'block' });
+        $('.menu-mobile-content-container').css({ 'display': 'none' });
+    });
+
     // Add smooth scrolling to all links
     $("a").on('click', function (event) {
 
--- wp-content/themes/portfolio/menu.php
+++ wp-content/themes/portfolio/menu.php
@@ -1,38 +1,65 @@
 <div class="headline">
-<nav>
-  <ul class="main-nav">
-    <li><a href="index.php" class="logo li-first"><img src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2017/12/logo-1.png" alt="AV logo"></a></li>
-      <li class="menu-desktop"><a href="#portfolio">Portfolio</a></li>
-      <li class="menu-desktop"><a href="#process">Process</a></li>
-      <li class="menu-desktop"><a href="#about">About</a></li>
-      <li class="menu-desktop"><a href="#contact" class="li-last">Contact</a></li>
-      
-      <li class="menu-mobile">menu</a></li>
-      <div class="menu-mobile-content-container">
-        <!-- <li class="menu-mobile-content-li"><a href="#portfolio">Portfolio</a></li>
-        <li class="menu-mobile-content-li"><a href="#process">Process</a></li>
-        <li class="menu-mobile-content-li"><a href="#about">About</a></li>
-        <li class="menu-mobile-content-li"><a href="#contact" class="li-last">Contact</a></li>   -->
-        <div class="menu-panel is-open">
-				<nav class="mobile-nav">
-					<ul class="mobile-menu-ul">
-						<li class="mobile-nav-li menu-blue">
-							<a href="#home">home</a>
-						</li>
-						<li class="mobile-nav-li menu-pink">
-							<a href="#work">work</a>
-						</li>
-						<li class="mobile-nav-li menu-yellow">
-							<a href="#about">about</a>
-						</li>
-						<li class="mobile-nav-li menu-green">
-							<a href="#contact">contact</a>
-						</li>
-					</ul>
-				</nav>
+	<nav>
+		<ul class="main-nav">
+			<li>
+				<a href="index.php" class="logo li-first">
+					<img src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2017/12/logo-1.png" alt="AV logo">
+				</a>
+			</li>
+			<li class="menu-desktop">
+				<a href="#portfolio">Portfolio</a>
+			</li>
+			<li class="menu-desktop">
+				<a href="#process">Process</a>
+			</li>
+			<li class="menu-desktop">
+				<a href="#about">About</a>
+			</li>
+			<li class="menu-desktop">
+				<a href="#contact" class="li-last">Contact</a>
+			</li>
+
+			<li class="menu-mobile">menu</a>
+			</li>
+			<div class="menu-mobile-content-container">
+				<!-- <div class="ul-mobile-menu-header">
+					 <li><a href="index.php" class="logo li-first"><img src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2017/12/logo-1.png" alt="AV logo"></a></li>
+				</div> -->
+				<ul class="mobile-menu-nav">
+					<li>
+						<a href="http://localhost:5757/GitHub/portoflio-2018/" class="logo li-first">
+							<img src="http://localhost:5757/GitHub/portoflio-2018/wp-content/uploads/2017/12/logo-1.png" alt="AV logo">
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="closing-cross-container">
+								<div class="cross-right"></div>
+								<div class="cross-left"></div>
+							</div>
+						</a>
+					</li>
+				</ul>
+				<div class="menu-panel">
+					<nav class="mobile-nav">
+						<ul class="mobile-menu-ul">
+							<li class="mobile-nav-li">
+								<a href="#portfolio">portfolio</a>
+							</li>
+							<li class="mobile-nav-li">
+								<a href="#process">process</a>
+							</li>
+							<li class="mobile-nav-li">
+								<a href="#about">about</a>
+							</li>
+							<li class="mobile-nav-li">
+								<a href="#contact">contact</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
 			</div>
-      </div>
-      
-      
-  </ul>
-</nav>
+
+
+		</ul>
+	</nav>
