36db90d3e9af3ec108b8c993769c619c753e9c39
andreviallon <viallon.andre@gmail.com>
2018-01-14 11:30:55 +0100

    update fixed header positioning

--- wp-content/themes/portfolio/menu.php
+++ wp-content/themes/portfolio/menu.php
@@ -1,66 +1,73 @@
 <div class="headline">
-	<nav>
-		<ul class="main-nav">
-			<li>
-				<a href="index.php" class="logo li-first">
-					<img src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/logo.png" alt="AV logo">
-				</a>
-			</li>
-			<li class="menu-desktop">
-				<a href="#portfolio">Portfolio</a>
-			</li>
-			<li class="menu-desktop">
-				<a href="#process">Process</a>
-			</li>
-			<li class="menu-desktop">
-				<a href="#about">About</a>
-			</li>
-			<li class="menu-desktop">
-				<a href="#contact" class="li-last">Contact</a>
-			</li>
+	<nav class="nav-container">
+				<div class="container">
+				<div class="row">
+		<ul class="main-nav col-xs-12">
 
-			<li class="menu-mobile"><div class="burger-menu">
-				<div class="burger-top"></div>
-				<div class="burger-mid"></div>
-				<div class="burger-bot"></div>
-			</div>
-			</li>
-			<div class="menu-mobile-content-container">
-				<ul class="mobile-menu-nav">
 					<li>
 						<a href="index.php" class="logo li-first">
 							<img src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/logo.png" alt="AV logo">
 						</a>
 					</li>
-					<li>
-						<a href="#">
-							<div class="closing-cross-container">
-								<div class="cross-right"></div>
-								<div class="cross-left"></div>
-							</div>
-						</a>
+					<li class="menu-desktop">
+						<a href="#portfolio">Portfolio</a>
 					</li>
-				</ul>
-				<div class="menu-panel">
-					<nav class="mobile-nav">
-						<ul class="mobile-menu-ul">
-							<li class="mobile-nav-li">
-								<a href="#portfolio">portfolio</a>
+					<li class="menu-desktop">
+						<a href="#process">Process</a>
+					</li>
+					<li class="menu-desktop">
+						<a href="#about">About</a>
+					</li>
+					<li class="menu-desktop">
+						<a href="#contact" class="li-last">Contact</a>
+					</li>
+
+					<li class="menu-mobile">
+						<div class="burger-menu">
+							<div class="burger-top"></div>
+							<div class="burger-mid"></div>
+							<div class="burger-bot"></div>
+						</div>
+					</li>
+					<div class="menu-mobile-content-container">
+						<ul class="mobile-menu-nav">
+							<li>
+								<a href="index.php" class="logo li-first">
+									<img src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/logo.png" alt="AV logo">
+								</a>
 							</li>
-							<li class="mobile-nav-li">
-								<a href="#process">process</a>
-							</li>
-							<li class="mobile-nav-li">
-								<a href="#about">about</a>
-							</li>
-							<li class="mobile-nav-li">
-								<a href="#contact">contact</a>
+							<li>
+								<a href="#">
+									<div class="closing-cross-container">
+										<div class="cross-right"></div>
+										<div class="cross-left"></div>
+									</div>
+								</a>
 							</li>
 						</ul>
-					</nav>
-				</div>
-			</div>
+						<div class="menu-panel">
+							<nav class="mobile-nav">
+								<ul class="mobile-menu-ul">
+									<li class="mobile-nav-li">
+										<a href="#portfolio">portfolio</a>
+									</li>
+									<li class="mobile-nav-li">
+										<a href="#process">process</a>
+									</li>
+									<li class="mobile-nav-li">
+										<a href="#about">about</a>
+									</li>
+									<li class="mobile-nav-li">
+										<a href="#contact">contact</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
 
+				</ul>
 
-		</ul>
+		</div>
+		</div>
+
 	</nav>
--- wp-content/themes/portfolio/post-local/bestseller.php
+++ wp-content/themes/portfolio/post-local/bestseller.php
@@ -1,16 +1,34 @@
 <div class="hide-if-open">
-<a href="http://localhost:5757/GitHub/portoflio-2018/bestseller-portal/">
-<img class="portfolio-img" src="http://localhost:5757/GitHub/portoflio-2018/wp-content/uploads/2017/12/Bestsetter-portfolio.png" alt="epico showcase" /></a>
-<h3 class="portfolio-title">Bestseller Portal</h3>
-<div class="portfolio-pink-bar"></div>
-<p class="portfolio-description">2017 / APP</p>
-</div><!--more-->
+  <a href="http://localhost:5757/GitHub/portoflio-2018/bestseller-portal/">
+    <img class="portfolio-img" src="http://localhost:5757/GitHub/portoflio-2018/wp-content/uploads/2017/12/Bestsetter-portfolio.png"
+      alt="epico showcase" />
+  </a>
+  <h3 class="portfolio-title">Bestseller Portal</h3>
+  <div class="portfolio-pink-bar"></div>
+  <p class="portfolio-description">2017 / APP</p>
+</div>
+<!--more-->
 
-<nav>
-<ul class="post-nav">
-<li><a href="http://localhost:5757/GitHub/portoflio-2018/" class="logo li-first"><img src="http://localhost:5757/GitHub/portoflio-2018/wp-content/uploads/2017/12/logo-1.png" alt="AV logo"></a></li>
-<li><a href="http://localhost:5757/GitHub/portoflio-2018/"><div class="closing-cross-container"><div class="cross-right"></div><div class="cross-left"></div></div></a></li>
-</ul>
+<nav class="nav-container">
+  <div class="container">
+    <div class="row">
+      <ul class="post-nav col-xs-12">
+        <li>
+          <a href="http://localhost:5757/GitHub/portoflio-2018/" class="logo li-first">
+            <img src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2017/12/logo-1.png" alt="AV logo">
+          </a>
+        </li>
+        <li>
+          <a href="http://localhost:5757/GitHub/portoflio-2018/">
+            <div class="closing-cross-container">
+              <div class="cross-right"></div>
+              <div class="cross-left"></div>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
 
 <div class="post-main-container col-xs-12">
@@ -27,8 +45,12 @@
   <div class="section-container title-container col-md-8 col-md-offset-2">
     <h2 class="post-section-title">overview</h2>
     <p class="section-content">
-       The aim of this project was to create a portal for Bestseller's employees, for them to have a better way of handling internal services such as orderings, bookings, requests, reporting incidents, and finding specific documents. To help the users fulfilling these goals, along with two other teammates, I created a prototype for a mobile application.
-    <br><br>See the Axure prototype at <a href="http://eixhe1.axshare.com/#g=1&p=home">http://eixhe1.axshare.com/#g=1&p=home</a>.</p>
+      The aim of this project was to create a portal for Bestseller's employees, for them to have a better way of handling internal
+      services such as orderings, bookings, requests, reporting incidents, and finding specific documents. To help the users
+      fulfilling these goals, along with two other teammates, I created a prototype for a mobile application.
+      <br>
+      <br>See the Axure prototype at
+      <a href="http://eixhe1.axshare.com/#g=1&p=home">http://eixhe1.axshare.com/#g=1&p=home</a>.</p>
   </div>
 
   <div class="section-container col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
@@ -37,7 +59,9 @@
 
   <div class="section-container title-container col-md-8 col-md-offset-2">
     <h2 class="post-section-title">process</h2>
-    <p class="section-content">The first step we took in this project was to do an in-depth analysis of the Bestseller brand as well as studying the material they provided us such as their target group and design guide. Thanks to the information collected we were able to create a persona to have a represent the typical user that will use our app.</p>
+    <p class="section-content">The first step we took in this project was to do an in-depth analysis of the Bestseller brand as well as studying the
+      material they provided us such as their target group and design guide. Thanks to the information collected we were
+      able to create a persona to have a represent the typical user that will use our app.</p>
   </div>
 
   <div class="section-container col-sm-12 col-sm-offset-0 col-md-6 col-md-offset-3">
@@ -47,35 +71,47 @@
 
   <div class="section-container title-container col-md-8 col-md-offset-2">
     <h2 class="post-section-title">user journeys</h2>
-    <p class="section-content">The next step, once we had the persona done, was to map out the flow of the app. To do so we create user journeys taking into consideration the different goals that the users want to achieve as well as the different paths they may take to get there. Thanks to this we had an idea about the information that needs to be displayed to the user and the layout of the app. </p>
+    <p class="section-content">The next step, once we had the persona done, was to map out the flow of the app. To do so we create user journeys taking
+      into consideration the different goals that the users want to achieve as well as the different paths they may take
+      to get there. Thanks to this we had an idea about the information that needs to be displayed to the user and the layout
+      of the app. </p>
   </div>
 
   <div class="section-container section-mobile col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
-    <img class="col-xs-12 col-xs-offset-0 col-md-6 col-md-offset-0" src="http://localhost:5757/GitHub/portoflio-2018/wp-content/uploads/2018/01/ideation1.jpg" alt="Bestseller User Journey">
-    <img class="col-xs-12 col-xs-offset-0 col-md-6 col-md-offset-0" src="http://localhost:5757/GitHub/portoflio-2018/wp-content/uploads/2018/01/ideation2.jpg" alt="Bestseller User Journey">
-  </div>  
+    <img class="col-xs-12 col-xs-offset-0 col-md-6 col-md-offset-0" src="http://localhost:5757/GitHub/portoflio-2018/wp-content/uploads/2018/01/ideation1.jpg"
+      alt="Bestseller User Journey">
+    <img class="col-xs-12 col-xs-offset-0 col-md-6 col-md-offset-0" src="http://localhost:5757/GitHub/portoflio-2018/wp-content/uploads/2018/01/ideation2.jpg"
+      alt="Bestseller User Journey">
+  </div>
 
   <div class="section-container title-container col-md-8 col-md-offset-2">
     <h2 class="post-section-title">next step</h2>
-    <p class="section-content">Next, we created wireframes based on the user journey we made and then mockups based on these wireframes. Once this was done, we created an interactive prototype for the bestseller representative to try our concept for the app.</p>
+    <p class="section-content">Next, we created wireframes based on the user journey we made and then mockups based on these wireframes. Once this was
+      done, we created an interactive prototype for the bestseller representative to try our concept for the app.</p>
   </div>
 
-    <div class="section-container section-mobile col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
-    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-home.png" alt="Bestseller Home">
-    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-service.png" alt="Bestseller service">
-    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-filter.png" alt="Bestseller filter">
-  </div>  
+  <div class="section-container section-mobile col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
+    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-home.png"
+      alt="Bestseller Home">
+    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-service.png"
+      alt="Bestseller service">
+    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-filter.png"
+      alt="Bestseller filter">
+  </div>
 
-    <div class="section-container section-mobile col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
-    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-booking.png" alt="Bestseller booking">
-    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-info-1.png" alt="Bestseller info">
-    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-verfication.png" alt="Bestseller verification">
-  </div>  
+  <div class="section-container section-mobile col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
+    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-booking.png"
+      alt="Bestseller booking">
+    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-info-1.png"
+      alt="Bestseller info">
+    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="http://localhost:8888/GitHub/portoflio-2018/wp-content/uploads/2018/01/bestseller-verfication.png"
+      alt="Bestseller verification">
+  </div>
 
   <div class="section-container title-container col-xs-12">
-      <div class="footer">
-    © 2017 Andre Viallon. All rights reserved.
-  </div>
+    <div class="footer">
+      © 2017 Andre Viallon. All rights reserved.
+    </div>
   </div>
 
 
