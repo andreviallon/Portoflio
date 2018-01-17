1427f135fca1bd407c09157d7037eec9165ae736
andreviallon <viallon.andre@gmail.com>
2018-01-17 14:31:27 +0100

    Update Epico pictures

--- wp-content/themes/portfolio/post-live/epico-live.php
+++ wp-content/themes/portfolio/post-live/epico-live.php
@@ -1,30 +1,17 @@
 <div class="hide-if-open">
-  <a href="http://localhost:5757/GitHub/portoflio-2018/epico-job-portal/">
-    <img class="portfolio-img" src="http://localhost:5757/GitHub/portoflio-2018/wp-content/uploads/2017/12/Epico-portfolio.png"
-      alt="epico showcase" />
-  </a>
-  <h3 class="portfolio-title">Epico Job Portal</h3>
-  <div class="portfolio-pink-bar"></div>
-  <p class="portfolio-description">2017 / WEP APP</p>
+<a href="https://www.portfolioandreviallon.com/epico-job-portal/">
+<img class="portfolio-img" src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/Epico-portfolio.png" alt="epico showcase" /></a>
+<h3 class="portfolio-title">Epico Job Portal</h3>
+<div class="portfolio-pink-bar"></div>
+<p class="portfolio-description">2017 / WEP APP</p>
 </div>
 <!--more-->
 <nav class="nav-container">
   <div class="container">
     <div class="row">
       <ul class="post-nav col-xs-12">
-        <li>
-          <a href="https://www.portfolioandreviallon.com/" class="logo li-first">
-            <img src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/logo.png" alt="AV logo">
-          </a>
-        </li>
-        <li>
-          <a href="https://www.portfolioandreviallon.com/">
-            <div class="closing-cross-container">
-              <div class="cross-right"></div>
-              <div class="cross-left"></div>
-            </div>
-          </a>
-        </li>
+<li><a href="https://www.portfolioandreviallon.com/" class="logo li-first"><img src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/logo.png" alt="AV logo"></a></li>
+<li><a href="https://www.portfolioandreviallon.com/"><div class="closing-cross-container"><div class="cross-right"></div><div class="cross-left"></div></div></a></li>
       </ul>
     </div>
   </div>
@@ -38,18 +25,12 @@
 
   <div class="section-container title-container col-md-8 col-md-offset-2">
     <h2 class="post-section-title">role</h2>
-    <p class="section-content job">UX / UI Design
-      <br>DesignFrontend Development</p>
+    <p class="section-content job">UX / UI Design<br>DesignFrontend Development</p>
   </div>
 
   <div class="section-container title-container col-md-8 col-md-offset-2">
     <h2 class="post-section-title">overview</h2>
-    <p class="section-content">Epico is a Danish IT consultancy company helping freelancers finding jobs and meeting recruiters. Epico wanted to create
-      a web-app allowing freelancers to see available jobs as well as blog post that Epico are posting. The web-app should
-      be responsive, user-friendly and facilitate the process of finding a job for freelancers.
-      <br>
-      <br>See all mockups on InVision at
-      <a href="https://invis.io/WCETXJA6Z">https://invis.io/WCETXJA6Z</a>.</p>
+    <p class="section-content">Epico is a Danish IT consultancy company helping freelancers finding jobs and meeting recruiters. Epico wanted to create a web-app allowing freelancers to see available jobs as well as blog post that Epico are posting. The web-app should be responsive, user-friendly and facilitate the process of finding a job for freelancers.<br><br>See all mockups on InVision at <a href="https://invis.io/WCETXJA6Z" target="_blank">https://invis.io/WCETXJA6Z</a>.</p>
   </div>
 
   <div class="section-container col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
@@ -58,51 +39,37 @@
 
   <div class="section-container title-container col-md-8 col-md-offset-2">
     <h2 class="post-section-title">process</h2>
-    <p class="section-content">To create this product, I first started by analysis Epico’s website to understand their values and what they which to
-      achieve with this web-app. Next, I listed all the different features that should be present asked by Epico such as
-      the user being able to login via LinkedIn and so on. Then, I created user journeys to understand the flow and the direction
-      the app should take. Finally, I started to do sketches and wireframes resulting in the mockups showcased.</p>
+    <p class="section-content">To create this product, I first started by analysis Epico’s website to understand their values and what they which to achieve with this web-app. Next, I listed all the different features that should be present asked by Epico such as the user being able to login via LinkedIn and so on. Then, I created user journeys to understand the flow and the direction the app should take. Finally, I started to do sketches and wireframes resulting in the mockups showcased.</p>
   </div>
 
   <div class="section-container col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
-    <img src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/epico-2.png" alt="Epico-2">
+    <img src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/Dashboard.png" alt="Epico-2">
   </div>
 
   <div class="section-container col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
-    <img src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/epico-3.png" alt="Epico-3">
-  </div>
+    <img src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/News-Details.png" alt="Epico-3">
+  </div>  
 
   <div class="section-container title-container col-md-8 col-md-offset-2">
     <h2 class="post-section-title">insight</h2>
-    <p class="section-content">To improve the usability on the mobile version, I focused on positioning every element so that they can be easily accessible
-      for the user using the ‘Thumb Zone’. For instance, all elements the user might click on the login page require a small
-      or no stretch for the user’s thumb. Also, the menu button is placed on the top left corner rather than the right one
-      because it is going to be used more frequently by the user rather than the notification button and so on.</p>
+    <p class="section-content">To improve the usability on the mobile version, I focused on positioning every element so that they can be easily accessible for the user using the ‘Thumb Zone’. For instance, all elements the user might click on the login page require a small or no stretch for the user’s thumb. Also, the menu button is placed on the top left corner rather than the right one because it is going to be used more frequently by the user rather than the notification button and so on.</p>
   </div>
 
   <div class="section-container section-mobile col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
-    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/epico-mobile-1.png"
-      alt="Epico mobile">
-    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/epico-mobile-2.png"
-      alt="Epico mobile">
-    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/epico-mobile-3.png"
-      alt="Epico mobile">
-  </div>
+    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/epico-mobile-1.png" alt="Epico mobile">
+    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/epico-mobile-2.png" alt="Epico mobile">
+    <img class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-0" src="https://www.portfolioandreviallon.com/wp-content/uploads/2018/01/epico-mobile-3.png" alt="Epico mobile">
+  </div>  
 
   <div class="section-container title-container col-md-8 col-md-offset-2">
     <h2 class="post-section-title">next step</h2>
-    <p class="section-content">Because of the short period of the project, 1 week for the whole design process, I did not have time to make any user
-      test and therefore would be a natural continuation to see how the users would interact with the solution created and
-      improve on it.
-      <br>
-      <br>See all mockups on InVision at
-      <a href="https://invis.io/WCETXJA6Z">https://invis.io/WCETXJA6Z</a>.</p>
+    <p class="section-content">Because of the short period of the project, 1 week for the whole design process, I did not have time to make any user test and therefore would be a natural continuation to see how the users would interact with the solution created and improve on it.<br><br>See all mockups on InVision at <a href="https://invis.io/WCETXJA6Z" target="_blank">https://invis.io/WCETXJA6Z</a>.</p>
   </div>
 
   <div class="section-container title-container col-xs-12">
-    <div class="footer">
-      © 2017 Andre Viallon. All rights reserved.
-    </div>
+      <div class="footer">
+    © 2017 Andre Viallon. All rights reserved.
+  </div>
   </div>
 
 
