e8b2b0a032b30d4b9f14e0e4c9f352bf02af1b8d
andreviallon <viallon.andre@gmail.com>
2018-01-28 13:56:10 +0100

    update mobile menu to display

--- wp-content/themes/portfolio/custom/script/script2.js
+++ wp-content/themes/portfolio/custom/script/script2.js
@@ -1,47 +1,46 @@
+$(document).ready(function() {
+  //Menu
+  //when menu is closed on click open it
+  $(".menu-mobile").click(function() {
+    $(".mobile-nav-toggle-cross").css({ display: "block" });
+    $(".menu-mobile").css({ display: "none" });
+    $(".menu-mobile-content-container").css({ display: "flex" });
+  });
+  //when menu is open on click close it
+  $(".mobile-menu-nav .closing-cross-container").click(function() {
+    $(".mobile-nav-toggle-cross").css({ display: "none" });
+    $(".menu-mobile").css({ display: "block" });
+    $(".menu-mobile-content-container").css({ display: "none" });
+  });
+  //when menu is open adn click on menu item close menu
+  $(".mobile-nav-li a").click(function() {
+    $(".mobile-nav-toggle-cross").css({ display: "none" });
+    $(".menu-mobile").css({ display: "block" });
+    $(".menu-mobile-content-container").css({ display: "none" });
+  });
 
-$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
+      // Store hash
+      var hash = this.hash;
 
-    //Menu 
-    //when menu is closed on click open it
-    $('.menu-mobile').click(function () {
-        $('.mobile-nav-toggle-cross').css({ 'display': 'block' });
-        $('.menu-mobile').css({ 'display': 'none' });
-        $('.menu-mobile-content-container').css({ 'display': 'flex' });
-    });
-    //when menu is open on click close it
-    $('.mobile-menu-nav .closing-cross-container').click(function () {
-        $('.mobile-nav-toggle-cross').css({ 'display': 'none' });
-        $('.menu-mobile').css({ 'display': 'block' });
-        $('.menu-mobile-content-container').css({ 'display': 'none' });
-    });
-    //when menu is open adn click on menu item close menu
-    $('.mobile-nav-li a').click(function () {
-        $('.mobile-nav-toggle-cross').css({ 'display': 'none' });
-        $('.menu-mobile').css({ 'display': 'block' });
-        $('.menu-mobile-content-container').css({ 'display': 'none' });
-    });
-
-    // Add smooth scrolling to all links
-    $("a").on('click', function (event) {
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-    });
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
 });
