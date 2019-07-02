2a40a300b6f668cbc070475a757d8b3490063cd1
Andre Viallon <viallon.andre@gmail.com>
2019-07-02 08:17:16 +0200

    Update Epico story

--- wp-content/themes/portfolio-live/post/asento.php
+++ wp-content/themes/portfolio-live/post/asento.php
@@ -8,7 +8,7 @@
 <!--more-->
 
 <nav class="nav-container">
-    <ul class="main-nav col-xs-12">
+    <ul class="main-nav col-sm-12">
         <div class="logo-container">
             <li>
                 <a href="http://localhost:8888/portoflio-2018" class="logo li-first">
@@ -29,51 +29,51 @@
     </ul>
 </nav>
 
-<div class="post-main-container col-xs-12">
-    <div class="post-title title-container col-xs-10 col-xs-offset-1">
+<div class="post-main-container">
+    <div class="post-title title-container col-sm-12 col-md-10 col-md-offset-1">
         <h1>Asento Learn</h1>
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">Role</h2>
         <p class="section-content job">UI / UX Design
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">Objective</h2>
         <p class="section-content">Design a product to help Marketing Coordinators learn and acquire new skills.</p>
         <a href="https://xd.adobe.com/view/56fa443c-71c8-4bc4-5fa5-a9b857bc7400-20cb/" target="_blank" class="menu-hover">Click here to see the prototype.</a>
 
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">The Problem</h2>
         <p class="section-content">Asento is a young and ambitious marketing company based in Kolding, Denmark and have for aim to disrupt the danish e-learning industry. Asento has a YouTube channel where they upload courses with very long videos, often over an hour long. This makes it hard for their followers to keep up with all the content and information in each video.</p>
     </div>
 
-    <div class="section-container col-xs-12">
+    <div class="section-container col-sm-12">
         <img src="https://www.andreviallon.com/wp-content/uploads/2018/08/Asento-thumbnail-1024x513.png" alt="Asento thumbnail">
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">The Solution</h2>
         <p class="section-content">Create easy to use and intuitive an e-learning platform to educate marketing coordinators, focusing on a desktop web-app. Because Marketing Coordinators already have busy schedule, the solution should have small videos grouped in courses so that the can easily find the information they need without having to what the entire course.</p>
         <p class="section-content">Moreover, the solution should include certificates that the users can add to their linkedin account to showcase their skills.</p>
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">Planning and research phase</h2>
         <p class="section-content">After defining the problem and identifying the solution, the next step was to design planning and research. In this phase, I research the competition, build a persona, sketch out ideas and build wireframes as well as user journeys that solves for user requirements.</p>
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <img src="https://www.andreviallon.com/wp-content/uploads/2018/08/Asento-Persona.png" alt="Asento Persona">
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">User Journey</h2>
     </div>
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <ul class="section-content ul-section-content">
             <h4>Context</h4>
             <li>Line works in a medium size company as a Marketing Coordinator</li>
@@ -101,23 +101,23 @@
         <img src="https://www.andreviallon.com/wp-content/uploads/2018/08/Asento-IA.png" alt="Asento IA">
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">Wireframes</h2>
         <p class="section-content">Once the research phase done, I started to make wireframes that I then used to do user tests, get feedback on my designs and improve on them.</p>
     </div>
 
-    <div class="section-container mockup-list col-xs-12">
+    <div class="section-container mockup-list col-sm-12">
         <img class="mockup-img" src="https://www.andreviallon.com/wp-content/uploads/2018/08/Home-1.png" alt="Asento wireframe home">
         <img class="mockup-img" src="https://www.andreviallon.com/wp-content/uploads/2018/08/Home-–-2.png" alt="Asento wireframe home 2">
         <img src="https://www.andreviallon.com/wp-content/uploads/2018/08/Course-List-–-2.png" alt="Asento wireframe home 3">
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">High Fidelity Mockups</h2>
         <p class="section-content">After brainstorming, sketching out ideas and creating rough wireframe, I started working on hi-fidelity mockups based on items gathered in the planning and research phase. The mockups should be based on Asento’s brand identity thus using the same typeface as well as colors but the solution should still have its own look and feel as it is meant to be a stand alone product.</p>
     </div>
 
-    <div class="section-container mockup-list col-xs-12">
+    <div class="section-container mockup-list col-sm-12">
         <img class="mockup-img" src="https://www.andreviallon.com/wp-content/uploads/2018/08/Landing-Page.png" alt="Asento Landing page">
         <img class="mockup-img" src="https://www.andreviallon.com/wp-content/uploads/2018/08/Home.png" alt="Asento Home">
         <img class="mockup-img" src="https://www.andreviallon.com/wp-content/uploads/2018/08/Library.png" alt="Asento Library">
@@ -125,14 +125,14 @@
         <img src="https://www.andreviallon.com/wp-content/uploads/2018/08/Certificates.png" alt="Asento Certificates">
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">Interactive Prototype</h2>
         <p class="section-content">After creating the essential screens and flow of the product based on the user requirements, the final step was to create an interactive prototype to continue user testing and enhance the user’s experience with the product.</p>
         <a href="https://xd.adobe.com/view/56fa443c-71c8-4bc4-5fa5-a9b857bc7400-20cb/" target="_blank" class="menu-hover">Click here to see the prototype.</a>
     </div>
 </div>
 
-<div class="post-footer section-container title-container col-xs-12">
+<div class="post-footer section-container title-container col-sm-12">
     <div class="footer">
         &copy; 2019 Andre Viallon. All rights reserved.
     </div>
--- wp-content/themes/portfolio-live/post/bestseller.php
+++ wp-content/themes/portfolio-live/post/bestseller.php
@@ -8,7 +8,7 @@
 <!--more-->
 
 <nav class="nav-container">
-    <ul class="main-nav col-xs-12">
+    <ul class="main-nav col-sm-12">
         <div class="logo-container">
             <li>
                 <a href="http://localhost:8888/portoflio-2018" class="logo li-first">
@@ -29,59 +29,62 @@
     </ul>
 </nav>
 
-<div class="post-main-container col-xs-12">
-    <div class="post-title title-container col-xs-10 col-xs-offset-1">
+<div class="post-main-container">
+    <div class="post-title title-container col-sm-12 col-md-12 col-md-offset-1">
         <h1>Bestseller Portal</h1>
     </div>
 
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">Role</h2>
         <p class="section-content job">UI / UX Design</p>
 
     </div>
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">Overview</h2>
         <p class="section-content"> The aim of this project was to create a portal for Bestseller's employees, for them to have a better way of handling internal services such as orderings, bookings, requests, reporting incidents, and finding specific documents. To help the users fulfilling these goals, along with two other teammates, I created a prototype for a mobile application.</p>
 
         <a href="https://xd.adobe.com/view/56fa443c-71c8-4bc4-5fa5-a9b857bc7400-20cb/" target="_blank" class="menu-hover">Click here to see the prototype.</a>
 
     </div>
-    <div class="section-container col-xs-12">
-        <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Bestseller-Portfolio-1024x513.png" alt="Bestseller-logo"></div>
-    <div class="section-container col-xs-10 col-xs-offset-1">
+
+    <div class="section-container col-sm-12">
+        <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Bestseller-Portfolio-1024x513.png" alt="Bestseller-logo">
+    </div>
+
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">Process</h2>
         <p class="section-content">The first step we took in this project was to do an in-depth analysis of the Bestseller brand as well as studying the material they provided us such as their target group and design guide. Thanks to the information collected we were able to create a persona to have a represent the typical user that will use our app.
 
     </div>
-    <div class="section-container col-xs-6 col-xs-offset-3">
-        <img src="https://www.andreviallon.com/wp-content/uploads/2018/07/bestseller-persona.png" alt="Bestseller-persona"></div>
-    <div class="section-container col-xs-10 col-xs-offset-1">
+
+    <div class="section-container col-sm-6 col-sm-offset-3">
+        <img src="https://www.andreviallon.com/wp-content/uploads/2018/07/bestseller-persona.png" alt="Bestseller-persona">
+    </div>
+
+    <div class="section-container col-sm-12 col-md-10 col-md-offset-1">
         <h2 class="post-section-title">User journeys</h2>
         <p class="section-content">The next step, once we had the persona done, was to map out the flow of the app. To do so we create user journeys taking into consideration the different goals that the users want to achieve as well as the different paths they may take to get there. Thanks to this we had an idea about the information that needs to be displayed to the user and the layout of the app.
 
     </div>
-    <div class="section-container reset-bottom-margin section-mobile col-xs-12">
-        <div class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-2 col-md-6 col-md-offset-0">
+    <div class="section-container reset-bottom-margin section-mobile col-sm-12">
+        <div class="col-sm-12 col-sm-offset-0 col-sm-10 col-sm-offset-2 col-md-6 col-md-offset-0">
             <img src="https://www.andreviallon.com/wp-content/uploads/2018/07/ideation1.jpg" alt="Bestseller User Journey"></div>
-        <div class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-2 col-md-6 col-md-offset-0">
+        <div class="col-sm-12 col-sm-offset-0 col-sm-10 col-sm-offset-2 col-md-6 col-md-offset-0">
             <img src="https://www.andreviallon.com/wp-content/uploads/2018/07/ideation2.jpg" alt="Bestseller User Journey"></div>
     </div>
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-10 col-sm-offset-1">
         <h2 class="post-section-title">Mockups</h2>
         <p class="section-content">Next, we created wireframes based on the user journey we made and then mockups based on these wireframes. Once this was done, we created an interactive prototype for the bestseller representative to try our concept for the app.
     </div>
-    <div class="section-container section-mobile flex-container col-xs-12">
+    <div class="section-container section-mobile flex-container col-sm-12">
         <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Home-–-Portfolio.png" alt="Bestseller Home">
         <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Services-Menu-–-Portfolio.png" alt="Bestseller Service">
         <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Facilities-Menu-–-Portfolio.png" alt="Bestseller Facilities Menu">
-        <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Services-List-–-Portfolio.png" alt="Bestseller Facilities List">
+        <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Facility-Details-–-Portfolio.png" alt="Bestseller Facility Details">
         <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Services-Filter-–-Portfolio.png" alt="Bestseller Filter">
         <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Services-Filter-Dates-–-Portfolio.png" alt="Bestseller Filter Date Picker">
-        <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Facility-Details-–-Portfolio.png" alt="Bestseller Facility Details">
-        <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Your-Order-–-Portfolio.png" alt="Bestseller Your Order">
-        <img src="https://www.andreviallon.com/wp-content/uploads/2018/09/Your-Order-Success-–-Portfolio.png" alt="Bestseller Confirmation"></div>
     </div>
-    <div class="section-container col-xs-10 col-xs-offset-1">
+    <div class="section-container col-sm-10 col-sm-offset-1">
         <h2 class="post-section-title">Interactive Prototype</h2>
         <p class="section-content">After creating the essential screens and flow of the app based on the knowledge acquired form our research, we created an interactive prototype to perform user tests and gather feedback to then reiterate on our design.</p>
         <a href="https://xd.adobe.com/view/56fa443c-71c8-4bc4-5fa5-a9b857bc7400-20cb/" target="_blank" class="menu-hover">Click here to see the prototype.</a>
@@ -89,7 +92,7 @@
     </div>
 </div>
 
-<div class="post-footer section-container title-container col-xs-12">
+<div class="post-footer section-container title-container col-sm-12">
     <div class="footer">
         &copy; 2019 Andre Viallon. All rights reserved.
     </div>
