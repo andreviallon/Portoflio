afc0c5eb3c51046ce398e2171b236053eccb0425
Andre Viallon <viallon.andre@gmail.com>
2019-09-18 13:34:48 +0200

    Update about text

--- wp-content/themes/portfolio-live/about.php
+++ wp-content/themes/portfolio-live/about.php
@@ -4,10 +4,10 @@
             <div class="about-text col-md-6">
                 <div class="about-title">Hi there!</div>
                 <div class="about-p">
-                    So, this is where I get to talk a bit about myself. My name is André. I am Software Engineer specialized in Frontend Development and UI / UX Design  with a thrilling passion for creating modern and beautiful solutions to make my user’s life easier.
+                    So, this is where I get to talk a bit about myself. My name is André. I am Software Engineer specialized in Frontend Development and UX Design  with a thrilling passion for creating modern and beautiful solutions to make my user’s life easier.
                     <div class="about-p">
                     </div>
-                    I’ve always been interested in challenging myself. That’s why I left France, my home country, when I was 17 for new adventures and moved to Denmark where I joined the International Baccalaureate. This is where I started to develop a strong interest in storytelling as well as human behavior by studying English Literature as well as Anthropology. 
+                    I’ve always been interested in challenging myself. That’s why I left France, my home country, when I was 17 for new adventures and moved to Denmark where I joined the International Baccalaureate. This is where I started to develop a strong interest in storytelling as well as human behavior by studying English Literature as well as Anthropology. I've also studied Web Development and UX Design at the International Business Academy in Kolding, Denmark.
                     <div class="about-p">
                     </div>
                     This combined with my curiosity for new technologies led me to become a UX Designer and study web development at the International Business Academy in Kolding, Denmark.
